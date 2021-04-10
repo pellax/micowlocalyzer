98ad9f042cad8a6551d56174485a08e79c4c6873
ashish.kshetri <ashish.kshetri@estudiantat.upc.edu>
2021-04-10 07:13:45 -0700

    Maps integrado con Frontend

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ function initMap(){
   map = new google.maps.Map(document.getElementById("map"),options)
   //Add Markers to Array
 
-  let MarkerArray = [ {location:{lat: 41.1189, lng: 1.2445}, 
+  let MarkerArray = [ {location:{lat: 41.1189, lng: 1.2445},
       content: `<h2>Tarragona</h2>`},
 
       {location:{lat: 41.9794, lng: 2.8214}, content: `<h2>Girona</h2>`},
@@ -31,17 +31,17 @@ function initMap(){
       const marker = new google.maps.Marker({
           position:property.location,
           map:map,
-    
+
           });
 
           // Check for custom Icon
 
-        
+
           if(property.content){
             const detailWindow = new google.maps.InfoWindow({
             content: property.content
           });
-  
+
             marker.addListener("mouseover", () =>{
                 detailWindow.open(map, marker);
             })
