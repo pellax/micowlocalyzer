2ea063deb25428b35546462ae7a152109197bfad
ALUMNE <alumne@spiderman.fib.upc.es>
2021-05-10 13:14:20 +0200

    cambio

--- myapp_v3/src/routes/localizar.routes.js
+++ myapp_v3/src/routes/localizar.routes.js
@@ -6,10 +6,10 @@ const { isAuthenticated} = require('../helpers/auth.js');
 
 router.get('/localizar', isAuthenticated, localizar);
 
-router.get('/locs', isAuthenticated, renderLocs);
+//router.get('/locs', isAuthenticated, renderLocs);
 
-router.get('/locs/add', isAuthenticated, renderLocsForm);
+//router.get('/locs/add', isAuthenticated, renderLocsForm);
 
-router.post('/locs/new-note', isAuthenticated, createNewLoc);
+//router.post('/locs/new-note', isAuthenticated, createNewLoc);
 
 module.exports = router;
