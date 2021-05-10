ae95db7a6afcc1203e7a3310e609b1b01ecd583d
ashish.kshetri <ashish.kshetri@estudiantat.upc.edu>
2021-05-10 03:42:53 -0700

    aaaa

--- myapp_v3/src/controllers/gps.controlers.js
+++ myapp_v3/src/controllers/gps.controlers.js
@@ -3,11 +3,25 @@ const gpsCtrl = {};
 const Gps = require("../models/Gps")
 
 gpsCtrl.sendgps = async (req,res) => {
-    const{id, lat, lon} = req.body;
+    try{
+        console.log(req.body);
+        const gps = await Gps.create(req.body);
+        
+        return res.status(201).json({
+            success: true,
+            data: gps
+        });
+    }
+    catch (err){
+        console.log(err);
+        res.status(500).json({error: 'Server error'});
+    }
+    // const{id, lat, lon} = req.body;
 
-    const newLoc = new Gps({id,lat,lon});
-    await newLoc.save();
-    res.send('recibido');
+    // const newLoc = new Gps({id,lat,lon});
+    // await newLoc.save();
+    // res.send('recibido');
 };
 
 module.exports = gpsCtrl;
+
