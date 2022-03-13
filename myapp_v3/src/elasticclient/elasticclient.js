d9901f8f84fb00353cd534a3cd91171a4701c151
pellax <tuxisalive@gmail.com>
2022-03-14 00:34:32 +0100

    a punto de funcionar

--- myapp_v3/src/elasticclient/elasticclient.js
+++ myapp_v3/src/elasticclient/elasticclient.js
@@ -3,7 +3,9 @@
 const { Client } = require('@elastic/elasticsearch')
 require("dotenv").config();
 module.exports.getClient = function(){
-const elasticUrl = process.env.ELASTIC_URL || "http://localhost:9200"
-const client = new Client({ node: elasticUrl});
+const elasticUrl = process.env.ELASTIC_URL || "https://localhost:9200"
+const client = new Client({ node: elasticUrl,auth:{username: 'elastic',password:'PleaseChangeMe'
+}
+});
 return client
 }
