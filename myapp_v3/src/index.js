601615b9a4c3b1f8f87761f5b4b601aa587ff331
pellax <tuxisalive@gmail.com>
2022-08-30 17:12:47 +0200

    Timeout correction for usability

--- myapp_v3/src/index.js
+++ myapp_v3/src/index.js
@@ -9,7 +9,7 @@ const createTab = async() =>  {
 createTables().catch(e => console.log(e))
 }
 
-setTimeout(createTab,25000);
+setTimeout(createTab,35000);
 
 app.listen(app.get('port'),() => {
     //const  misetup = setup.createTables().catch(e => console.log(e))
